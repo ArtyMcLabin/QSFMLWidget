004bb01181712e3d4aa6e723c212ceac076d1dfa
Arty Party <ArtyMcLabin@users.noreply.github.com>
2016-09-24 16:58:05 +0300

    Update qsfmlwidget.cpp

--- qsfml/qsfmlwidget.cpp
+++ qsfml/qsfmlwidget.cpp
@@ -1,5 +1,4 @@
 /* Copyright (c) 2015 Arty McLabin
- * http://www.facebook.com/Arty.McLabin
  * SFML drawzone Qt widget-class for free use by you, fellow - OOP gamedevs.
  * Based on GNKW's "QSFMLCanvas" abstract class, and implements it as a convinient
  * ready-to-use widget class along other UI objects, like QOpenGLWidget.
@@ -7,7 +6,7 @@
  *** it more useful and convinient, less pointless (as some say).
  **  feel free to use and edit the code at this file, yet please, be thankful and
  **  don't forget to mention other's effort in your project as long as you are using
- **  their code parts. Good Luck fgt!
+ **  their code parts. Good Luck!
  '*********************************************************************************/
 
 
